1ffaca01204aa6b991973808b69d0afff4ba6757
FenilP07 <fenil2755@gmail.com>
2026-03-12 11:10:49 -0400

    implemented a multer middleware to upload files

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -1,1 +1,23 @@
 import multer from "multer";
+import path from "path";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, callback) => {
+  const allowedExtensions = /pdf|docx|doc/;
+  const isExtValid = allowedExtensions.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+  const isMimeValid = allowedExtensions.test(file.mimetype);
+
+  if (isExtValid && isMimeValid) {
+    return callback(null, true);
+  }
+  callback(new Error("Invalid file type"));
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
